6cdbc6c20360aba7d6cd646c2048206186a39534
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-04-01 19:11:05 -0700

    everyhting added

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reducers from './reducers';
+import ReduxThunk from 'redux-thunk';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import LoginForm from './LoginForm';
+
+class App extends React.Component {
+  componentWillMount(){
+    var config = {
+      apiKey: "AIzaSyAJKaDy95qBFdyo7JGlyKJ5dBebfaQNUeM",
+      authDomain: "swipe-app-646be.firebaseapp.com",
+      databaseURL: "https://swipe-app-646be.firebaseio.com",
+      projectId: "swipe-app-646be",
+      storageBucket: "swipe-app-646be.appspot.com",
+      messagingSenderId: "77507712093"
+    };
+    firebase.initializeApp(config);
+    firebase.firestore();
+  }
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+    <Provider store={store}>
+      <View style={{flex:1}}>
+        <LoginForm />
+      </View>
+    </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ LoginForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Container, Header, Content, Form, Item, Input, Label, Button } from 'native-base';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import {loginUser, passwordChanged, emailChanged} from './actions';
+
+class LoginForm extends Component {
+  constructor(props) {
+  super(props);
+  this.state = { email: '', password: '' };
+}
+
+  onButtonPress() {
+    const userData = {
+    email: this.state.email,
+    password: this.state.password
+  };
+  this.props.loginUser(userData)
+ }
+
+
+  render() {
+    return (
+      <Container >
+        <Header />
+        <Content contentContainerStyle={{flex: 1}} style={{padding: 20}}>
+          <Form style={{alignItems: 'center'}}>
+            <Item floatingLabel>
+              <Label>Email</Label>
+              <Input
+                 onChangeText={(text) => this.setState({email: text})}
+                 value={this.state.email}
+                />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input
+                onChangeText={(text) => this.setState({password: text})}
+                value={this.state.password}
+              />
+            </Item>
+            <Button full bordered info
+            style={{marginTop: 20}}
+             onPress={this.onButtonPress.bind(this)}
+             >
+              <Text>Submit</Text>
+            </Button>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    error: state.auth.error
+  }
+}
+
+export default connect(mapStateToProps, { loginUser, passwordChanged, emailChanged })(LoginForm);
